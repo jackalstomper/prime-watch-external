3388baa162c31badad9bd113c5abc8a87b5810ea
icefire <erifeci@gmail.com>
2019-03-10 14:38:45 -0400

    Add emulated memory start location support
    Drop Cygwin dependency for windows builds
    Have server update at 30 ticks per second
    Add option to listen only to loopback interface

---
+++ common.cpp
@@ -0,0 +1,39 @@
+#ifdef WIN32
+#include <intrin.h>
+#include <cstdint>
+
+uint32_t beToHost32(uint32_t bigEndian) {
+    return _byteswap_ulong(bigEndian);
+}
+
+uint32_t hostToBe32(uint32_t value) {
+    return _byteswap_ulong(value);
+}
+
+uint64_t beToHost64(uint64_t bigEndian) {
+    return _byteswap_uint64(bigEndian);
+}
+
+uint64_t hostToBe64(uint64_t value) {
+    return _byteswap_uint64(value);
+}
+
+#else
+
+uint32_t beToHost32(uint32_t bigEndian) {
+    return be32toh(bigEndian);
+}
+
+uint32_t hostToBe32(uint32_t value) {
+    return htobe32(value);
+}
+
+uint64_t beToHost64(uint64_t bigEndian) {
+    return be64toh(bigEndian);
+}
+
+uint64_t hostToBe64(uint64_t value) {
+    return htobe64(value);
+}
+
+#endif
--- common.h
+++ common.h
@@ -1,8 +1,15 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-void handleClient(int sock);
-void wootWootGetRoot();
+#ifdef WIN32
+#   include <cstdint>
+#endif
+
 void findAndAttachProcess();
+
+uint32_t beToHost32(uint32_t bigEndian);
+uint32_t hostToBe32(uint32_t value);
+uint64_t beToHost64(uint64_t bigEndian);
+uint64_t hostToBe64(uint64_t value);
 
 #endif
